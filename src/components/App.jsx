225e162510d203149ed5cae666f0f2424c8bf793
alex-k22 <114990357+alex-k22@users.noreply.github.com>
2023-06-19 22:24:00 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { useState } from 'react';
+import Section from './Section/Section';
+import FeedbackOption from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+import css from './App.module.css';
+
+export  function App()  {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const leaveFeedback = feedbackName => {
+    switch (feedbackName) {
+      case "good":
+        setGood(prevState => prevState + 1);
+        break;
+      case "neutral":
+        setNeutral(state => state +1);
+        break;
+      case "bad":
+        setBad(state => state +1);
+        break;
+      default:
+        return;    
+    }
+  }
+
+  const countTotalFeedback = () => good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = () => {
+        return Math.round((good * 100) / (good + neutral + bad));
+  };
+
+    return (
+      <div className={css.container}>
+        <Section title="Please live feedback">
+          <FeedbackOption
+            options={["good", "neutral", "bad"]}
+            onLeaveFeedback={leaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positivePercentage={countPositiveFeedbackPercentage()}
+            ></Statistics>
+          )}
+        </Section>
+      </div>
+    );
+  }
+
